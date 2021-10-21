ee659466437a965e5f2bfd6ff87440ab9dc79a7a
Patrick Ohly <patrick.ohly@intel.com>
2021-10-21 19:44:50 +0200

    klogr: fork klog output formatting, use it for testing logger
    This originally was proposed as enhancement for klog in
    https://github.com/kubernetes/klog/pull/240: the klog output formatting gets
    turned into utility code which then can be used with a bit of boilerplate code
    in a LogSink implementation.
    
    The helper code from https://github.com/kubernetes/klog/pull/240/ was
    de-duplicating keys because that was what the original klogr code did.
    
    This de-duplication gets removed here because:
    - Neither klog nor zap do that.
    - It hides a problem in invalid log calls (keys should be unique).
    - The code did massive allocations because each append() for the
      final slice created a new slice.
    - It did not handle missing values as intended (inserted a nil value
      instead of MISSING).

---
+++ staging/src/k8s.io/klogr/logger/buffer.go
@@ -0,0 +1,109 @@
+/*
+Copyright 2013 Google Inc. All Rights Reserved.
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package logger
+
+import (
+	"bytes"
+	"sync"
+)
+
+// buffer holds a byte Buffer for reuse. The zero value is ready for use.
+type buffer struct {
+	bytes.Buffer
+	tmp  [64]byte // temporary byte array for creating headers.
+	next *buffer
+}
+
+var (
+	// freeList is a list of byte buffers, maintained under freeListMu.
+	freeList *buffer
+	// freeListMu maintains the free list. It is separate from the main mutex
+	// so buffers can be grabbed and printed to without holding the main lock,
+	// for better parallelization.
+	freeListMu sync.Mutex
+)
+
+// getBuffer returns a new, ready-to-use buffer.
+func getBuffer() *buffer {
+	freeListMu.Lock()
+	b := freeList
+	if b != nil {
+		freeList = b.next
+	}
+	freeListMu.Unlock()
+	if b == nil {
+		b = new(buffer)
+	} else {
+		b.next = nil
+		b.Reset()
+	}
+	return b
+}
+
+// putBuffer returns a buffer to the free list.
+func putBuffer(b *buffer) {
+	if b.Len() >= 256 {
+		// Let big buffers die a natural death.
+		return
+	}
+	freeListMu.Lock()
+	b.next = freeList
+	freeList = b
+	freeListMu.Unlock()
+}
+
+// Some custom tiny helper functions to print the log header efficiently.
+
+const digits = "0123456789"
+
+// twoDigits formats a zero-prefixed two-digit integer at buf.tmp[i].
+func (buf *buffer) twoDigits(i, d int) {
+	buf.tmp[i+1] = digits[d%10]
+	d /= 10
+	buf.tmp[i] = digits[d%10]
+}
+
+// nDigits formats an n-digit integer at buf.tmp[i],
+// padding with pad on the left.
+// It assumes d >= 0.
+func (buf *buffer) nDigits(n, i, d int, pad byte) {
+	j := n - 1
+	for ; j >= 0 && d > 0; j-- {
+		buf.tmp[i+j] = digits[d%10]
+		d /= 10
+	}
+	for ; j >= 0; j-- {
+		buf.tmp[i+j] = pad
+	}
+}
+
+// someDigits formats a zero-prefixed variable-width integer at buf.tmp[i].
+func (buf *buffer) someDigits(i, d int) int {
+	// Print into the top, then copy down. We know there's space for at least
+	// a 10-digit number.
+	j := len(buf.tmp)
+	for {
+		j--
+		buf.tmp[j] = digits[d%10]
+		d /= 10
+		if d == 0 {
+			break
+		}
+	}
+	return copy(buf.tmp[i:], buf.tmp[j:])
+}
