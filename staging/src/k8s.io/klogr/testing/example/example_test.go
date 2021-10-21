796082cff909c9b06eb5b2ad5a59dca06f20090a
Patrick Ohly <patrick.ohly@intel.com>
2021-10-21 19:44:50 +0200

    klogr: fork klog output formatting, use it for testing logger
    This originally was proposed as enhancement for klog in
    https://github.com/kubernetes/klog/pull/240: the klog output formatting gets
    turned into utility code which then can be used with a bit of boilerplate code
    in a LogSink implementation.
    
    The helper code from https://github.com/kubernetes/klog/pull/240/ was
    de-duplicating keys because that was what the original klogr code did.
    
    This de-duplication gets removed here because:
    - Neither klog nor zap do that.
    - It hides a problem in invalid log calls (keys should be unique).
    - The code did massive allocations because each append() for the
      final slice created a new slice.
    - It did not handle missing values as intended (inserted a nil value
      instead of MISSING).

---
+++ staging/src/k8s.io/klogr/testing/example/example_test.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package example
+
+import (
+	"fmt"
+	"testing"
+
+	"k8s.io/klogr"
+	"k8s.io/klogr/internal/test"
+	ktesting "k8s.io/klogr/testing"
+)
+
+func TestMain(m *testing.M) {
+	ktesting.TestMainWithLogging(m)
+}
+
+func TestKlogr(t *testing.T) {
+	logger, _ := ktesting.NewTestContext(t)
+	exampleOutput(logger)
+}
+
+type pair struct {
+	a, b int
+}
+
+func (p pair) String() string {
+	return fmt.Sprintf("(%d, %d)", p.a, p.b)
+}
+
+var _ fmt.Stringer = pair{}
+
+type err struct {
+	msg string
+}
+
+func (e err) Error() string {
+	return "failed: " + e.msg
+}
+
+var _ error = err{}
+
+func exampleOutput(logger klogr.Logger) {
+	logger.Info("hello world")
+	logger.Error(err{msg: "some error"}, "failed")
+	logger.V(1).Info("verbosity 1")
+	logger.WithName("main").WithName("helper").Info("with prefix")
+	obj := test.KMetadataMock{Name: "joe", NS: "kube-system"}
+	logger.Info("key/value pairs",
+		"int", 1,
+		"float", 2.0,
+		"pair", pair{a: 1, b: 2},
+		"raw", obj,
+		"kobj", klogr.KObj(obj),
+	)
+	logger.V(4).Info("info message level 4")
+	logger.V(5).Info("info message level 5")
+	logger.V(6).Info("info message level 6")
+}
