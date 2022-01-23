d5b5a33198d1a622a03a847c36574e9ad3dd4f3d
Patrick Ohly <patrick.ohly@intel.com>
2022-01-23 11:10:48 +0100

    WIP: schdeduler_perf: support JSON

--- test/integration/scheduler_perf/main_test.go
+++ test/integration/scheduler_perf/main_test.go
@@ -17,11 +17,38 @@ limitations under the License.
 package benchmark
 
 import (
+	"flag"
+	"fmt"
+	"os"
 	"testing"
 
+	"github.com/spf13/pflag"
+
 	"k8s.io/kubernetes/test/integration/framework"
+	"k8s.io/component-base/logs"
+	_ "k8s.io/component-base/logs/json/register"
 )
 
 func TestMain(m *testing.M) {
+	o := logs.NewOptions()
+
+	// This whole boilerplate code will be simpler if the utility code for
+	// contextual logging is merged.
+
+	// component-base only supports pflag at the moment.
+	var fs pflag.FlagSet
+	o.AddFlags(&fs)
+	// Not ideal. https://github.com/spf13/pflag/pull/330 would be better.
+	fs.VisitAll(func(f *pflag.Flag) {
+		if flag.CommandLine.Lookup(f.Name) == nil {
+			flag.CommandLine.Var(f.Value, f.Name, f.Usage)
+		}
+	})
+	flag.Parse()
+	if err := o.ValidateAndApply(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
 	framework.EtcdMain(m.Run)
 }
