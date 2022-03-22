306496ebb9fee82b2f14b8d90caf5f4e396fd9c5
Patrick Ohly <patrick.ohly@intel.com>
2022-03-22 16:56:49 +0100

    kube-controller-manager: add ResourceClaim controller
    The controller uses the exact same logic as the generic ephemeral inline volume
    controller, just for inline ResourceClaimTemplate -> ResourceClaim.
    
    In addition, it supports removal of pods from the ReservedFor field when those
    pods are known to not need the claim anymore. At the moment, only this special
    case is supported. Removal of arbitrary objects would imply granting full read
    access to all types to determine whether a) an object is gone and b) if the
    current incarnation is the one which is listed in ReservedFor. This may get
    added later.

--- cmd/kube-controller-manager/app/options/options_test.go
+++ cmd/kube-controller-manager/app/options/options_test.go
@@ -56,6 +56,7 @@ import (
 	podgcconfig "k8s.io/kubernetes/pkg/controller/podgc/config"
 	replicasetconfig "k8s.io/kubernetes/pkg/controller/replicaset/config"
 	replicationconfig "k8s.io/kubernetes/pkg/controller/replication/config"
+	resourceclaimconfig "k8s.io/kubernetes/pkg/controller/resourceclaim/config"
 	resourcequotaconfig "k8s.io/kubernetes/pkg/controller/resourcequota/config"
 	serviceaccountconfig "k8s.io/kubernetes/pkg/controller/serviceaccount/config"
 	statefulsetconfig "k8s.io/kubernetes/pkg/controller/statefulset/config"
@@ -88,6 +89,7 @@ var args = []string{
 	"--concurrent-statefulset-syncs=15",
 	"--concurrent-endpoint-syncs=10",
 	"--concurrent-ephemeralvolume-syncs=10",
+	"--concurrent-resource-claim-syncs=10",
 	"--concurrent-service-endpoint-syncs=10",
 	"--concurrent-gc-syncs=30",
 	"--concurrent-namespace-syncs=20",
@@ -291,6 +293,11 @@ func TestAddFlags(t *testing.T) {
 		EphemeralVolumeController: &EphemeralVolumeControllerOptions{
 			&ephemeralvolumeconfig.EphemeralVolumeControllerConfiguration{
 				ConcurrentEphemeralVolumeSyncs: 10,
+			},
+		},
+		ResourceClaimController: &ResourceClaimControllerOptions{
+			&resourceclaimconfig.ResourceClaimControllerConfiguration{
+				ConcurrentResourceClaimSyncs: 10,
 			},
 		},
 		GarbageCollectorController: &GarbageCollectorControllerOptions{
@@ -550,6 +557,9 @@ func TestApplyTo(t *testing.T) {
 			},
 			EphemeralVolumeController: ephemeralvolumeconfig.EphemeralVolumeControllerConfiguration{
 				ConcurrentEphemeralVolumeSyncs: 10,
+			},
+			ResourceClaimController: resourceclaimconfig.ResourceClaimControllerConfiguration{
+				ConcurrentResourceClaimSyncs: 10,
 			},
 			GarbageCollectorController: garbagecollectorconfig.GarbageCollectorControllerConfiguration{
 				ConcurrentGCSyncs: 30,
