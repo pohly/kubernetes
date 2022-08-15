8b75453f6d21665a1126ff10c9b7a5668c764c20
Patrick Ohly <patrick.ohly@intel.com>
2022-08-15 13:43:28 +0200

    e2e: add test driver for dynamic resource allocation
    The driver can be used manually against a cluster started with
    local-up-cluster.sh and is also used for E2E testing. Because the tests proxy
    connections from the nodes into the e2e.test binary and create/delete files via
    the equivalent of "kubectl exec dd/rm", they can be run against arbitrary
    clusters. Each test gets its own driver instance and resource class, therefore
    they can run in parallel.

---
+++ test/e2e/dra/test-driver/app/kubeletplugin.go
@@ -0,0 +1,178 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package app
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	cdiapi "github.com/container-orchestrated-devices/container-device-interface/pkg/cdi"
+	specs "github.com/container-orchestrated-devices/container-device-interface/specs-go"
+	"k8s.io/component-helpers/dra/kubeletplugin"
+	"k8s.io/klog/v2"
+	drapbv1 "k8s.io/kubelet/pkg/apis/dra/v1alpha1"
+)
+
+type ExamplePlugin struct {
+	logger  klog.Logger
+	d       kubeletplugin.DRAPlugin
+	fileOps FileOperations
+
+	cdiDir     string
+	driverName string
+}
+
+var _ drapbv1.NodeServer = &ExamplePlugin{}
+
+// getJSONFilePath returns the absolute path where CDI file is/should be.
+func (ex *ExamplePlugin) getJSONFilePath(claimUID string) string {
+	return filepath.Join(ex.cdiDir, fmt.Sprintf("%s-%s.json", ex.driverName, claimUID))
+}
+
+// FileOperations defines optional callbacks for handling CDI files.
+type FileOperations struct {
+	// Create must overwrite the file.
+	Create func(name string, content []byte) error
+
+	// Remove must remove the file. It must not return an error when the
+	// file does not exist.
+	Remove func(name string) error
+}
+
+// StartPlugin sets up the servers that are necessary for a DRA kubelet plugin.
+func StartPlugin(logger klog.Logger, cdiDir, driverName string, fileOps FileOperations, opts ...kubeletplugin.Option) (*ExamplePlugin, error) {
+	if fileOps.Create == nil {
+		fileOps.Create = func(name string, content []byte) error {
+			return os.WriteFile(name, content, os.FileMode(0644))
+		}
+	}
+	if fileOps.Remove == nil {
+		fileOps.Remove = func(name string) error {
+			if err := os.Remove(name); err != nil && !os.IsNotExist(err) {
+				return err
+			}
+			return nil
+		}
+	}
+	ex := &ExamplePlugin{
+		logger:     logger,
+		fileOps:    fileOps,
+		cdiDir:     cdiDir,
+		driverName: driverName,
+	}
+
+	opts = append(opts,
+		kubeletplugin.Logger(logger),
+		kubeletplugin.DriverName(driverName),
+	)
+	d, err := kubeletplugin.Start(ex, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("start kubelet plugin: %v", err)
+	}
+	ex.d = d
+
+	return ex, nil
+}
+
+// stop ensures that all servers are stopped and resources freed.
+func (ex *ExamplePlugin) Stop() {
+	ex.d.Stop()
+}
+
+func (ex *ExamplePlugin) IsRegistered() bool {
+	status := ex.d.RegistrationStatus()
+	if status == nil {
+		return false
+	}
+	return status.PluginRegistered
+}
+
+// NodePrepareResource ensures that the CDI file for the claim exists. It uses
+// a deterministic name to simplify NodeUnprepareResource (no need to remember
+// or discover the name) and idempotency (when called again, the file simply
+// gets written again).
+func (ex *ExamplePlugin) NodePrepareResource(ctx context.Context, req *drapbv1.NodePrepareResourceRequest) (*drapbv1.NodePrepareResourceResponse, error) {
+	logger := klog.FromContext(ctx)
+
+	// Determine environment variables.
+	var env map[string]string
+	if err := json.Unmarshal([]byte(req.ResourceHandle), &env); err != nil {
+		return nil, fmt.Errorf("unmarshal resource handle: %v", err)
+	}
+
+	// CDI wants env variables as set of strings.
+	envs := []string{}
+	for key, val := range env {
+		envs = append(envs, key+"="+val)
+	}
+
+	deviceName := "claim-" + req.ClaimUid
+	spec := specs.Spec{
+		// TODO: clarify what version to put here (https://github.com/container-orchestrated-devices/container-device-interface/issues/60).
+		Version: "0.2.0",
+		Kind:    ex.driverName + "/test",
+		// At least one device is required and its entry must have more
+		// than just the name.
+		Devices: []specs.Device{
+			{
+				Name: deviceName,
+				ContainerEdits: specs.ContainerEdits{
+					Env: envs,
+				},
+			},
+		},
+	}
+	filePath := ex.getJSONFilePath(req.ClaimUid)
+	cdiSpec, err := cdiapi.NewSpec(&spec, filePath, 1)
+	if err != nil {
+		return nil, fmt.Errorf("create spec: %v", err)
+	}
+
+	buffer, err := json.Marshal(*cdiSpec)
+	if err != nil {
+		return nil, fmt.Errorf("marshal spec: %v", err)
+	}
+
+	// TODO: use a new cdi lib and its write interface once a decision
+	// about transient files has been reached (see
+	// https://github.com/container-orchestrated-devices/container-device-interface/pull/77)
+	if err := ex.fileOps.Create(filePath, buffer); err != nil {
+		return nil, fmt.Errorf("failed to write CDI file %v", err)
+	}
+
+	dev := cdiSpec.GetDevice(deviceName).GetQualifiedName()
+	resp := &drapbv1.NodePrepareResourceResponse{CdiDevice: []string{dev}}
+
+	logger.V(3).Info("CDI file created", "path", filePath, "device", dev)
+	return resp, nil
+}
+
+// NodeUnprepareResource removes the CDI file created by
+// NodePrepareResource. It's idempotent, therefore it is not an error when that
+// file is already gone.
+func (ex *ExamplePlugin) NodeUnprepareResource(ctx context.Context, req *drapbv1.NodeUnprepareResourceRequest) (*drapbv1.NodeUnprepareResourceResponse, error) {
+	logger := klog.FromContext(ctx)
+
+	filePath := ex.getJSONFilePath(req.ClaimUid)
+	if err := ex.fileOps.Remove(filePath); err != nil {
+		return nil, fmt.Errorf("error removing CDI file: %v", err)
+	}
+	logger.V(3).Info("CDI file removed", "path", filePath)
+	return &drapbv1.NodeUnprepareResourceResponse{}, nil
+}
