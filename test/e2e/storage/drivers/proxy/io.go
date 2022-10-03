425e451ade0f7bec96fb74df83fdda39725f2991
Patrick Ohly <patrick.ohly@intel.com>
2022-10-03 21:16:16 +0200

    e2e storage: avoid usage of stdin for file creation
    It turned out to be unreliable (see
    https://github.com/kubernetes/kubernetes/issues/112834).  Encoding the data
    inside the command as input for base64 is a workaround that is fine for small
    amounts of data. It becomes less efficient and/or unusable for large amounts.

--- test/e2e/storage/drivers/proxy/io.go
+++ test/e2e/storage/drivers/proxy/io.go
@@ -17,6 +17,7 @@ limitations under the License.
 package proxy
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io"
 
@@ -65,9 +66,23 @@ func (p PodDirIO) Mkdir(path string) error {
 }
 
 func (p PodDirIO) CreateFile(path string, content io.Reader) error {
-	_, stderr, err := p.execute([]string{"dd", "of=" + path}, content)
+	// Piping the content into dd via stdin turned out to be unreliable.
+	// Sometimes dd would stop after writing zero bytes, without an error
+	// from ExecWithOptions (reported as
+	// https://github.com/kubernetes/kubernetes/issues/112834).
+	//
+	// Therefore the content is now encoded inside the command itself.
+	data, err := io.ReadAll(content)
 	if err != nil {
-		return fmt.Errorf("dd of=%s: stderr=%q, %v", path, stderr, err)
+		return fmt.Errorf("read content: %v", err)
+	}
+	encoded := make([]byte, base64.StdEncoding.EncodedLen(len(data)))
+	base64.StdEncoding.Encode(encoded, data)
+	_, stderr, err := p.execute([]string{"sh", "-c", fmt.Sprintf(`base64 -d >'%s' <<EOF
+%s
+EOF`, path, string(encoded))}, nil)
+	if err != nil {
+		return fmt.Errorf("decoding into %q: stderr=%q, %v", path, stderr, err)
 	}
 	return nil
 }
