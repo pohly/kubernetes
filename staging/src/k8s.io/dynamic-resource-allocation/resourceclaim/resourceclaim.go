kubernetes/dynamic-resource-allocation

staging: add ResourceClaim support code
This is similar to the support code for generic ephemeral inline volumes.
Differences:
- to avoid stuttering, the functions are just resourceclaim.Name and
  resourceclaim.IsForPod
- resourceclaim.Name returns the right name for both cases (template
  and reference), which will simplify some code

Other helper functions check some simple status conditions.

Kubernetes-commit: 59d46c026f377534f365ad030eb1c1606676f2bd

resourceclaim/resourceclaim.go
@@ -1,0 +1,81 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package resourceclaim provides code that supports the usual pattern
+// for accessing the ResourceClaim that is referenced by a PodResourceClaim:
+//
+// - determine the ResourceClaim name that corresponds to the PodResourceClaim
+// - retrieve the ResourceClaim
+// - verify that the ResourceClaim is owned by the pod if generated from a template
+// - use the ResourceClaim
+package resourceclaim
+
+import (
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	resourcev1alpha1 "k8s.io/api/resource/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Name returns the name of the ResourceClaim object that gets referenced by or
+// created for the PodResourceClaim. The name is deterministic and therefore
+// this function does not need any additional information and it will never
+// fail.
+//
+// Either podClaim.ResourceClaimName or podClaim.Template must be non-nil, but not
+// both. This is enforced by API validation.
+//
+// If podClaim.Template is not nil, the caller must check that the
+// ResourceClaim is indeed the one that was created for the Pod by calling
+// IsUsable.
+func Name(pod *v1.Pod, podClaim *v1.PodResourceClaim) string {
+	if podClaim.Source.ResourceClaimName != nil {
+		return *podClaim.Source.ResourceClaimName
+	}
+	return pod.Name + "-" + podClaim.Name
+}
+
+// IsForPod checks that the ResourceClaim is the ephemeral volume that
+// was created for the Pod. It returns an error that is informative
+// enough to be returned by the caller without adding further details
+// about the Pod or ResourceClaim.
+func IsForPod(pod *v1.Pod, claim *resourcev1alpha1.ResourceClaim) error {
+	// Checking the namespaces is just a precaution. The caller should
+	// never pass in a ResourceClaim that isn't from the same namespace as the
+	// Pod.
+	if claim.Namespace != pod.Namespace || !metav1.IsControlledBy(claim, pod) {
+		return fmt.Errorf("ResourceClaim %s/%s was not created for pod %s/%s (pod is not owner)", claim.Namespace, claim.Name, pod.Namespace, pod.Name)
+	}
+	return nil
+}
+
+// IsReservedForPod checks whether a claim lists the pod as one of the objects
+// that the claim was reserved for.
+func IsReservedForPod(pod *v1.Pod, claim *resourcev1alpha1.ResourceClaim) bool {
+	for _, reserved := range claim.Status.ReservedFor {
+		if reserved.UID == pod.UID {
+			return true
+		}
+	}
+	return false
+}
+
+// CanBeReserved checks whether the claim could be reserved for another object.
+func CanBeReserved(claim *resourcev1alpha1.ResourceClaim) bool {
+	return claim.Status.Allocation.Shareable ||
+		len(claim.Status.ReservedFor) == 0
+}

resourceclaim/resourceclaim_test.go
@@ -1,0 +1,127 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resourceclaim
+
+import (
+	"fmt"
+	"testing"
+
+	v1 "k8s.io/api/core/v1"
+	resourcev1alpha1 "k8s.io/api/resource/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func TestResourceClaimIsForPod(t *testing.T) {
+	uid := 0
+	newUID := func() types.UID {
+		uid++
+		return types.UID(fmt.Sprintf("%d", uid))
+	}
+	isController := true
+
+	podNotOwner := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "kube-system",
+			Name:      "podNotOwner",
+			UID:       newUID(),
+		},
+	}
+	podOwner := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "kube-system",
+			Name:      "podOwner",
+			UID:       newUID(),
+		},
+	}
+	claimNoOwner := &resourcev1alpha1.ResourceClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "kube-system",
+			Name:      "claimNoOwner",
+			UID:       newUID(),
+		},
+	}
+	claimWithOwner := &resourcev1alpha1.ResourceClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "kube-system",
+			Name:      "claimNoOwner",
+			UID:       newUID(),
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					UID:        podOwner.UID,
+					Controller: &isController,
+				},
+			},
+		},
+	}
+	userClaimWithOwner := &resourcev1alpha1.ResourceClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "user-namespace",
+			Name:      "userClaimWithOwner",
+			UID:       newUID(),
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					UID:        podOwner.UID,
+					Controller: &isController,
+				},
+			},
+		},
+	}
+
+	testcases := map[string]struct {
+		pod           *v1.Pod
+		claim         *resourcev1alpha1.ResourceClaim
+		expectedError string
+	}{
+		"owned": {
+			pod:   podOwner,
+			claim: claimWithOwner,
+		},
+		"other-pod": {
+			pod:           podNotOwner,
+			claim:         claimWithOwner,
+			expectedError: `ResourceClaim kube-system/claimNoOwner was not created for pod kube-system/podNotOwner (pod is not owner)`,
+		},
+		"no-owner": {
+			pod:           podOwner,
+			claim:         claimNoOwner,
+			expectedError: `ResourceClaim kube-system/claimNoOwner was not created for pod kube-system/podOwner (pod is not owner)`,
+		},
+		"different-namespace": {
+			pod:           podOwner,
+			claim:         userClaimWithOwner,
+			expectedError: `ResourceClaim user-namespace/userClaimWithOwner was not created for pod kube-system/podOwner (pod is not owner)`,
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			err := IsForPod(tc.pod, tc.claim)
+			if tc.expectedError == "" {
+				if err != nil {
+					t.Errorf("expected no error, got %v", err)
+				}
+			} else {
+				if err == nil {
+					t.Errorf("expected error %q, got nil", tc.expectedError)
+				} else if tc.expectedError != err.Error() {
+					t.Errorf("expected error %q, got %v", tc.expectedError, err)
+				}
+			}
+		})
+	}
+}

pohly/kubernetes

staging/src/k8s.io/dynamic-resource-allocation/resourceclaim/resourceclaim.go
@@ -1,0 +1,80 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package resourceclaim provides code that supports the usual pattern
+// for accessing the ResourceClaim that is referenced by a PodResourceClaim:
+//
+// - determine the ResourceClaim name that corresponds to the PodResourceClaim
+// - retrieve the ResourceClaim
+// - verify that the ResourceClaim is owned by the pod if generated from a template
+// - use the ResourceClaim
+package resourceclaim
+
+import (
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Name returns the name of the ResourceClaim object that gets referenced by or
+// created for the PodResourceClaim. The name is deterministic and therefore
+// this function does not need any additional information and it will never
+// fail.
+//
+// Either podClaim.ResourceClaimName or podClaim.Template must be non-nil, but not
+// both. This is enforced by API validation.
+//
+// If podClaim.Template is not nil, the caller must check that the
+// ResourceClaim is indeed the one that was created for the Pod by calling
+// IsUsable.
+func Name(pod *v1.Pod, podClaim *v1.PodResourceClaim) string {
+	if podClaim.Claim.ResourceClaimName != nil {
+		return *podClaim.Claim.ResourceClaimName
+	}
+	return pod.Name + "-" + podClaim.Name
+}
+
+// IsForPod checks that the ResourceClaim is the ephemeral volume that
+// was created for the Pod. It returns an error that is informative
+// enough to be returned by the caller without adding further details
+// about the Pod or ResourceClaim.
+func IsForPod(pod *v1.Pod, claim *v1.ResourceClaim) error {
+	// Checking the namespaces is just a precaution. The caller should
+	// never pass in a ResourceClaim that isn't from the same namespace as the
+	// Pod.
+	if claim.Namespace != pod.Namespace || !metav1.IsControlledBy(claim, pod) {
+		return fmt.Errorf("ResourceClaim %s/%s was not created for pod %s/%s (pod is not owner)", claim.Namespace, claim.Name, pod.Namespace, pod.Name)
+	}
+	return nil
+}
+
+// IsReservedForPod checks whether a claim lists the pod as one of the objects
+// that the claim was reserved for.
+func IsReservedForPod(pod *v1.Pod, claim *v1.ResourceClaim) bool {
+	for _, reserved := range claim.Status.ReservedFor {
+		if reserved.UID == pod.UID {
+			return true
+		}
+	}
+	return false
+}
+
+// CanBeReserved checks whether the claim could be reserved for another object.
+func CanBeReserved(claim *v1.ResourceClaim) bool {
+	return claim.Status.Allocation.SharedResource ||
+		len(claim.Status.ReservedFor) == 0
+}

staging/src/k8s.io/dynamic-resource-allocation/resourceclaim/resourceclaim_test.go
@@ -1,0 +1,126 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resourceclaim
+
+import (
+	"fmt"
+	"testing"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func TestResourceClaimIsForPod(t *testing.T) {
+	uid := 0
+	newUID := func() types.UID {
+		uid++
+		return types.UID(fmt.Sprintf("%d", uid))
+	}
+	isController := true
+
+	podNotOwner := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "kube-system",
+			Name:      "podNotOwner",
+			UID:       newUID(),
+		},
+	}
+	podOwner := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "kube-system",
+			Name:      "podOwner",
+			UID:       newUID(),
+		},
+	}
+	claimNoOwner := &v1.ResourceClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "kube-system",
+			Name:      "claimNoOwner",
+			UID:       newUID(),
+		},
+	}
+	claimWithOwner := &v1.ResourceClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "kube-system",
+			Name:      "claimNoOwner",
+			UID:       newUID(),
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					UID:        podOwner.UID,
+					Controller: &isController,
+				},
+			},
+		},
+	}
+	userClaimWithOwner := &v1.ResourceClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "user-namespace",
+			Name:      "userClaimWithOwner",
+			UID:       newUID(),
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					UID:        podOwner.UID,
+					Controller: &isController,
+				},
+			},
+		},
+	}
+
+	testcases := map[string]struct {
+		pod           *v1.Pod
+		claim         *v1.ResourceClaim
+		expectedError string
+	}{
+		"owned": {
+			pod:   podOwner,
+			claim: claimWithOwner,
+		},
+		"other-pod": {
+			pod:           podNotOwner,
+			claim:         claimWithOwner,
+			expectedError: `ResourceClaim kube-system/claimNoOwner was not created for pod kube-system/podNotOwner (pod is not owner)`,
+		},
+		"no-owner": {
+			pod:           podOwner,
+			claim:         claimNoOwner,
+			expectedError: `ResourceClaim kube-system/claimNoOwner was not created for pod kube-system/podOwner (pod is not owner)`,
+		},
+		"different-namespace": {
+			pod:           podOwner,
+			claim:         userClaimWithOwner,
+			expectedError: `ResourceClaim user-namespace/userClaimWithOwner was not created for pod kube-system/podOwner (pod is not owner)`,
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			err := IsForPod(tc.pod, tc.claim)
+			if tc.expectedError == "" {
+				if err != nil {
+					t.Errorf("expected no error, got %v", err)
+				}
+			} else {
+				if err == nil {
+					t.Errorf("expected error %q, got nil", tc.expectedError)
+				} else if tc.expectedError != err.Error() {
+					t.Errorf("expected error %q, got %v", tc.expectedError, err)
+				}
+			}
+		})
+	}
+}