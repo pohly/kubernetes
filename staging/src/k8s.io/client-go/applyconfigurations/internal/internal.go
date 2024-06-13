59acb6526489d7ada3b2c752492c63a741324cf8
Patrick Ohly <patrick.ohly@intel.com>
2024-06-13 17:25:39 +0200

    DRA: remove immediate allocation
    As agreed in https://github.com/kubernetes/enhancements/pull/4709, immediate
    allocation is one of those features which can be removed because it makes no
    sense for structured parameters and the justification for classic DRA is weak.

--- staging/src/k8s.io/client-go/applyconfigurations/internal/internal.go
+++ staging/src/k8s.io/client-go/applyconfigurations/internal/internal.go
@@ -12397,9 +12397,6 @@ var schemaYAML = typed.YAMLObject(`types:
 - name: io.k8s.api.resource.v1alpha2.ResourceClaimSpec
   map:
     fields:
-    - name: allocationMode
-      type:
-        scalar: string
     - name: parametersRef
       type:
         namedType: io.k8s.api.resource.v1alpha2.ResourceClaimParametersReference
