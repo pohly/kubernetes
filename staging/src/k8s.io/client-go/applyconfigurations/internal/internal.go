73df0080e18958bfefdd12b9b3de49680d8d338b
Patrick Ohly <patrick.ohly@intel.com>
2024-06-13 18:43:17 +0200

    DRA: remove "sharable" from claim allocation result
    Now all claims are shareable up to the limit imposed by the size of the
    "reserverFor" array.
    
    This is one of the agreed simplifications for 1.31.

--- staging/src/k8s.io/client-go/applyconfigurations/internal/internal.go
+++ staging/src/k8s.io/client-go/applyconfigurations/internal/internal.go
@@ -12144,9 +12144,6 @@ var schemaYAML = typed.YAMLObject(`types:
           elementType:
             namedType: io.k8s.api.resource.v1alpha2.ResourceHandle
           elementRelationship: atomic
-    - name: shareable
-      type:
-        scalar: boolean
 - name: io.k8s.api.resource.v1alpha2.DriverAllocationResult
   map:
     fields:
@@ -12365,9 +12362,6 @@ var schemaYAML = typed.YAMLObject(`types:
       type:
         namedType: io.k8s.apimachinery.pkg.apis.meta.v1.ObjectMeta
       default: {}
-    - name: shareable
-      type:
-        scalar: boolean
 - name: io.k8s.api.resource.v1alpha2.ResourceClaimParametersReference
   map:
     fields:
