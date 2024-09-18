kubernetes/dynamic-resource-allocation

DRA scheduler: also pre-compute the unique ResourceSlice.NodeName
Converting a node's name once to a unique string and then converting to many
unique names is faster than memory comparisons.

    goos: linux
    goarch: amd64
    pkg: k8s.io/kubernetes/test/integration/scheduler_perf
    cpu: Intel(R) Core(TM) i9-7980XE CPU @ 2.60GHz
                                                                                       │            before            │                       after                        │
                                                                                       │ SchedulingThroughput/Average │ SchedulingThroughput/Average  vs base              │
    PerfScheduling/SchedulingWithResourceClaimTemplateStructured/5000pods_500nodes-36                      36.65 ± 2%                     36.89 ± 2%       ~ (p=0.452 n=6)
    PerfScheduling/SteadyStateClusterResourceClaimTemplateStructured/empty_100nodes-36                     106.7 ± 3%                     105.7 ± 5%       ~ (p=0.701 n=6)
    PerfScheduling/SteadyStateClusterResourceClaimTemplateStructured/empty_500nodes-36                     119.7 ± 3%                     117.8 ± 3%       ~ (p=0.084 n=6)
    PerfScheduling/SteadyStateClusterResourceClaimTemplateStructured/half_100nodes-36                      121.1 ± 4%                     119.5 ± 4%       ~ (p=0.297 n=6)
    PerfScheduling/SteadyStateClusterResourceClaimTemplateStructured/half_500nodes-36                      63.72 ± 3%                     63.22 ± 2%       ~ (p=0.485 n=6)
    PerfScheduling/SteadyStateClusterResourceClaimTemplateStructured/full_100nodes-36                      110.2 ± 2%                     109.5 ± 2%       ~ (p=0.258 n=6)
    PerfScheduling/SteadyStateClusterResourceClaimTemplateStructured/full_500nodes-36                      28.16 ± 5%                     27.56 ± 5%       ~ (p=0.513 n=6)
    geomean                                                                                                73.15         

structured/allocator.go
@@ -839,7 +839,7 @@ func (alloc *allocator) createNodeSelector(result []internalDeviceResult) (*v1.N
 
 	for i := range result {
 		slice := result[i].slice
-		if slice.Spec.NodeName != "" {
+		if slice.Spec.NodeName != draapi.NullUniqueString {
 			// At least one device is local to one node. This
 			// restricts the allocation to that node.
 			return &v1.NodeSelector{
@@ -847,7 +847,7 @@ func (alloc *allocator) createNodeSelector(result []internalDeviceResult) (*v1.N
 					MatchFields: []v1.NodeSelectorRequirement{{
 						Key:      "metadata.name",
 						Operator: v1.NodeSelectorOpIn,
-						Values:   []string{slice.Spec.NodeName},
+						Values:   []string{slice.Spec.NodeName.String()},
 					}},
 				}},
 			}, nil

pohly/kubernetes

     │            before            │                     after                     │
     │ SchedulingThroughput/Average │ SchedulingThroughput/Average  vs base         │
                         53.52 ± 6%                     53.51 ± 7%  ~ (p=1.000 n=6)
     │           before            │                    after                     │
     │ SchedulingThroughput/Perc50 │ SchedulingThroughput/Perc50  vs base         │
                       17.00 ± 12%                    17.50 ± 9%  ~ (p=0.225 n=6)
     │ SchedulingThroughput/Perc90 │ SchedulingThroughput/Perc90  vs base         │
                       119.5 ± 11%                   121.0 ± 13%  ~ (p=0.682 n=6)
     │ SchedulingThroughput/Perc95 │ SchedulingThroughput/Perc95  vs base         │
                       124.0 ± 13%                   126.0 ± 13%  ~ (p=0.983 n=6)
     │ SchedulingThroughput/Perc99 │ SchedulingThroughput/Perc99  vs base         │
                       129.0 ± 14%                   129.0 ± 14%  ~ (p=0.972 n=6)
     │     before      │              after               │
     │ runtime_seconds │ runtime_seconds  vs base         │
            92.87 ± 5%        91.91 ± 6%  ~ (p=0.372 n=6)

staging/src/k8s.io/dynamic-resource-allocation/structured/allocator.go
@@ -814,7 +814,7 @@ func (alloc *allocator) createNodeSelector(result []internalDeviceResult) (*v1.N
 
 	for i := range result {
 		slice := result[i].slice
-		if slice.Spec.NodeName != "" {
+		if slice.Spec.NodeName != api.NullUniqueString {
 			// At least one device is local to one node. This
 			// restricts the allocation to that node.
 			return &v1.NodeSelector{
@@ -822,7 +822,7 @@ func (alloc *allocator) createNodeSelector(result []internalDeviceResult) (*v1.N
 					MatchFields: []v1.NodeSelectorRequirement{{
 						Key:      "metadata.name",
 						Operator: v1.NodeSelectorOpIn,
-						Values:   []string{slice.Spec.NodeName},
+						Values:   []string{slice.Spec.NodeName.String()},
 					}},
 				}},
 			}, nil