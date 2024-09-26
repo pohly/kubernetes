61b3b2c47cb5fd22861dedc1d821d6e6a14928cc
Patrick Ohly <patrick.ohly@intel.com>
2024-09-26 16:56:48 +0200

    DRA API: use DeviceCapacity struct instead of plain Quantity
    This enables a future extension where capacity of a single device gets consumed
    by different claims. The semantic without any additional fields is the same as
    before: a capacity cannot be split up and is only an attribute of a device.
    
    Because its semantically the same as before, two-way conversion to v1alpha3 is
    possible.

--- staging/src/k8s.io/client-go/applyconfigurations/internal/internal.go
+++ staging/src/k8s.io/client-go/applyconfigurations/internal/internal.go
@@ -12645,7 +12645,7 @@ var schemaYAML = typed.YAMLObject(`types:
       type:
         map:
           elementType:
-            namedType: io.k8s.apimachinery.pkg.api.resource.Quantity
+            namedType: io.k8s.api.resource.v1beta1.DeviceCapacity
 - name: io.k8s.api.resource.v1beta1.CELDeviceSelector
   map:
     fields:
@@ -12709,6 +12709,12 @@ var schemaYAML = typed.YAMLObject(`types:
     - name: version
       type:
         scalar: string
+- name: io.k8s.api.resource.v1beta1.DeviceCapacity
+  map:
+    fields:
+    - name: quantity
+      type:
+        namedType: io.k8s.apimachinery.pkg.api.resource.Quantity
 - name: io.k8s.api.resource.v1beta1.DeviceClaim
   map:
     fields:
