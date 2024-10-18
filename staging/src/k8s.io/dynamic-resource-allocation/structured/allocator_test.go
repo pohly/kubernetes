a95e09b781f2aa47bca78bdc707b213859f77b12
Patrick Ohly <patrick.ohly@intel.com>
2024-10-18 16:30:05 +0200

    DRA: first test case for partitionable devices
    This demonstrates that the scheduler currently ignores the unknown device type.

--- staging/src/k8s.io/dynamic-resource-allocation/structured/allocator_test.go
+++ staging/src/k8s.io/dynamic-resource-allocation/structured/allocator_test.go
@@ -198,11 +198,26 @@ func device(name string, capacity map[resourceapi.QualifiedName]resource.Quantit
 	return device
 }
 
+// generate a CompositeDevice object with the given name, capacity and attributes.
+func compositeDevice(name string, capacity map[resourceapi.QualifiedName]resource.Quantity, attributes map[resourceapi.QualifiedName]resourceapi.DeviceAttribute) resourceapi.Device {
+	device := resourceapi.Device{
+		Name: name,
+		Composite: &resourceapi.CompositeDevice{
+			Attributes: attributes,
+		},
+	}
+	device.Composite.Capacity = make(map[resourceapi.QualifiedName]resourceapi.DeviceCapacity, len(capacity))
+	for name, quantity := range capacity {
+		device.Composite.Capacity[name] = resourceapi.DeviceCapacity{Quantity: quantity}
+	}
+	return device
+}
+
 // generate a ResourceSlice object with the given name, node,
-// driver and pool names, generation and a list of devices.
+// driver and pool names, generation and a list of devices or mixins
 // The nodeSelection parameter may be a string (= node name),
 // true (= all nodes), or a node selector (= specific nodes).
-func slice(name string, nodeSelection any, pool, driver string, devices ...resourceapi.Device) *resourceapi.ResourceSlice {
+func slice(name string, nodeSelection any, pool, driver string, devicesOrMixins ...any) *resourceapi.ResourceSlice {
 	slice := &resourceapi.ResourceSlice{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
@@ -214,8 +229,18 @@ func slice(name string, nodeSelection any, pool, driver string, devices ...resou
 				ResourceSliceCount: 1,
 				Generation:         1,
 			},
-			Devices: devices,
 		},
+	}
+
+	for _, item := range devicesOrMixins {
+		switch item := item.(type) {
+		case resourceapi.Device:
+			slice.Spec.Devices = append(slice.Spec.Devices, item)
+		case resourceapi.DeviceMixin:
+			slice.Spec.DeviceMixins = append(slice.Spec.DeviceMixins, item)
+		default:
+			panic(fmt.Sprintf("unexpected item: want resourceapi.Device or resourceapi.DeviceMixin, got %T", item))
+		}
 	}
 
 	switch nodeSelection := nodeSelection.(type) {
@@ -374,6 +399,19 @@ func TestAllocator(t *testing.T) {
 				localNodeSelector(node1),
 				deviceAllocationResult(req0, driverA, pool1, device1, false),
 			)},
+		},
+		"composite-device": {
+			claimsToAllocate: objects(claim(claim0, req0, classA)),
+			classes:          objects(class(classA, driverA)),
+			slices: objects(slice(slice1, node1, pool1, driverA,
+				compositeDevice(device1, map[resourceapi.QualifiedName]resource.Quantity{
+					"memory": resource.MustParse("1Gi"),
+				}, nil),
+			)),
+			node: node(node1, region1),
+
+			// TODO: support allocating composite devices
+			expectResults: nil,
 		},
 		"other-node": {
 			claimsToAllocate: objects(claim(claim0, req0, classA)),
